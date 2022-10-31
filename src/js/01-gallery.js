4c279587679998147a502c301534d3705d9d2e6c
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-10-31 16:23:56 +0200

    main

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
-
-console.log(galleryItems);
+const divEL =  document.querySelector(".gallery");
+function addImg(img) {
+  img.map(el => divEL.insertAdjacentHTML('afterbegin',`
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      alt=${el.description}
+    />
+  </a>`))
+return img;
+}
+console.log(addImg(galleryItems));
+new SimpleLightbox('.gallery a',{
+  captionsData: "alt",
+  captionDelay: 250,
+});
