2b6a55a0b9430f98428739e8f6c9e141d5cfe9d5
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-11-01 17:42:34 +0200

    main

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,7 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Change code below this line
 const divEL =  document.querySelector(".gallery");
 function addImg(img) {
   img.map(el => divEL.insertAdjacentHTML('afterbegin',`
